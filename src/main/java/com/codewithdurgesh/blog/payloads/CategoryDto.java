666727bbf5d92a83f66c4416eb6d8d55c45a5d5d
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-06-17 18:57:44 +0530

    bolg-app-apis

--- src/main/java/com/codewithdurgesh/blog/payloads/CategoryDto.java
+++ src/main/java/com/codewithdurgesh/blog/payloads/CategoryDto.java
@@ -1,5 +1,8 @@
 package com.codewithdurgesh.blog.payloads;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +13,13 @@ import lombok.Setter;
 public class CategoryDto {
 	
 	private Integer categoryId;
+	
+	@NotBlank
+	@Size(min = 4,message = "Min size of category title is 4")
 	private String categoryTitle;
+	
+	@NotBlank
+	@Size(min = 10,message = "Min size of category desc is 10")
 	private String categoryDescription;
 	
 
