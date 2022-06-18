c25a3dc3d3565b01d7c33f216121d0a7bf45531f
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-06-18 19:12:27 +0530

    blog-app-apis

---
+++ src/main/java/com/codewithdurgesh/blog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codewithdurgesh.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codewithdurgesh.blog.entities.Post;
+import com.codewithdurgesh.blog.payloads.PostDto;
+import com.codewithdurgesh.blog.services.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+	
+	@Autowired
+	private PostService postService;
+	
+	//create
+	
+	@PostMapping("/user/{userId}/category/{categoryId}/posts")
+	public ResponseEntity<PostDto> createPost(
+			@RequestBody PostDto postDto,
+			@PathVariable Integer userId,
+			@PathVariable Integer categoryId
+			)
+	{
+	PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
+	return new ResponseEntity<PostDto>(createPost,HttpStatus.CREATED);
+	}
+}
